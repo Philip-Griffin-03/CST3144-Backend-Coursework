bae0ab2b998a667851dfa0a80d792a63f2692ddd
Philip-Griffin <115720667+Philip-Griffin@users.noreply.github.com>
2025-11-18 15:23:07 +0000

    half of backend done, connceted to mongo and aws with first route

--- express.js
+++ express.js
@@ -1,19 +1,30 @@
+
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
 
+var cors = require("cors");
+var { MongoClient } = require("mongodb");
+
 
 var app = express();
 
-const uri = "mongodb+srv://philipgriffin03_db_user:<6w£pW)m13%3f>@cluster0.rfwrbbl.mongodb.net/"
+const uri = "mongodb+srv://philipgriffin03_db_user:6w%C2%A3pW%29m13%253f@cluster0.rfwrbbl.mongodb.net/"
+const dbname = "";
 
+let db;
+
+app.use(cors());//allows frontend to call api
+app.use(express.json());//parses requests as json
 
 app.use(function(req, res, next) {//use in code
     console.log("Request IP: " + req.url);
     console.log("Request date: " + new Date());
     next();
 });
-app.use(function(req, res, next) {
+
+
+app.use("/img", function(req, res, next) {
     var filePath = path.join(__dirname, "img", req.url);
     fs.stat(filePath, function(err, fileInfo) {
     if (err) {
@@ -27,15 +38,27 @@ app.use(function(req, res, next) {
     }
     });
 });
-app.use(function(req, res) {
-    res.status(404);
-    res.send("File not found!");
+
+
+
+
+
+
+
+
+
+app.get("/lessons", async (req, res) => {
+    try {
+        const lessons = await db.collection("lessons").find({}).toArray();
+        res.json(lessons);
+    } catch (err) {
+        console.error("Error fetching lessons:", err);
+        res.status(500).json({ error: "Failed to fetch lessons" });
+    }
 });
 
 
-app.get("/lessons", function(req, res){
 
-});
 
 app.post("/checkout", function(req, res){
 
@@ -50,10 +73,10 @@ app.put("/lessons", function(req, res){
 
 
 
-
-
-
-
+app.use(function(req, res) {//change around
+    res.status(404);
+    res.send("File not found!");
+});
 
 
 
